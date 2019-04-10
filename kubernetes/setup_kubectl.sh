4fca068d136a6a6e07addd4a67fc94974f944f3d
Rupendra <rbandyopadhyay@i-360.com>
2019-04-10 17:24:16 -0400

    Added / Modified steps for Kubernetes

--- kubernetes/setup_kubectl.sh
+++ kubernetes/setup_kubectl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
--- kubernetes/start_cluster.sh
+++ kubernetes/start_cluster.sh
@@ -7,6 +7,24 @@ then
 fi
 
 kubeadm init --pod-network-cidr=10.244.0.0/16
+sleep 10
 
-kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/canal/rbac.yaml
-kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/canal/canal.yaml
+cd $(dirname $0)
+./setup_kubectl.sh
+
+success=1
+while [ $success -ne 0 ]
+do
+  kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/canal/rbac.yaml
+  success=$?
+  sleep 5
+done
+
+success=1
+while [ $success -ne 0 ]
+do
+  kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/canal/canal.yaml
+  success=$?
+  sleep 5
+done
+
